89034ceb83e565f07852d045488e39b6b11c12ff
Aeon Katana <aeonkatana@gmail.com>
2022-03-13 17:37:18 +0800

    Fixed some task ui

---
+++ src/main/resources/static/js/pnltask.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function(){
+	
+	$("#myform").submit(function(e){
+		e.preventDefault();
+		alert("lol");
+	});
+	
+	$(".btncancel").click(function(){
+		$(this).parent().parent().parent().remove();
+		$(this).parent().parent().parent().remove();
+		window.parent.document.getElementById('framebtn').removeAttribute("disabled");
+		window.parent.document.getElementById('myframe').setAttribute("height", 0);
+	});
+});
